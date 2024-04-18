9f03367e948edbc5073e3bda37b25c788ab3ed48
MuhammadSyauqiAlBashir <bashirsyauqi@gmail.com>
2024-04-18 18:16:49 +0700

    Api clearance

--- vehicare-app/src/app/api/vehicle/route.ts
+++ vehicare-app/src/app/api/vehicle/route.ts
@@ -1,25 +1,21 @@
-
 import ModelStatus from "@/databases/models/status";
 import ModelVehicle from "@/databases/models/vehicles";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    try {
-        const body = await req.json()
-        body.UserId = "6620cea3918910ac27d8e7b0" //example
-        const res = await ModelVehicle.addVehicle(body)
-
-        // Default Status
-        await ModelStatus.addStatus(res.insertedId)
-
-        return NextResponse.json(
-            { success: `Success added vehicle` },
-            { status: 201 }
-        )
-    } catch (error: any) {
-        return NextResponse.json(
-            { error: "Internal Server Error" },
-            { status: 500 }
-        )
-    }
+  try {
+    const body = await req.json();
+    body.UserId = req.headers.get("x-user-id") as string;
+    const res = await ModelVehicle.addVehicle(body);
+    await ModelStatus.addStatus(res.insertedId);
+    return NextResponse.json(
+      { success: `Success added vehicle` },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
