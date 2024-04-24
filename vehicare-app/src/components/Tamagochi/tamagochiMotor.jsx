4983e8cfd7267490e712bd9ca7f72573548f4d80
ghamz-id <hcoustic@gmail.com>
2024-04-25 01:22:00 +0700

    Layouting Home

--- vehicare-app/src/components/Tamagochi/tamagochiMotor.jsx
+++ vehicare-app/src/components/Tamagochi/tamagochiMotor.jsx
@@ -6,96 +6,129 @@ import { Status } from "@/databases/models/types";
 import { AddStatus, changeGatcha } from "@/actions/status";
 
 export default function TamagochiMotor({ selectedVehicle }) {
-  const id = selectedVehicle;
-  const [vehicle, setVehicle] = useState({});
-  const [loading, setLoading] = useState(false);
+	const id = selectedVehicle;
+	const [vehicle, setVehicle] = useState({});
+	const [loading, setLoading] = useState(false);
 
-  const fetchVehicle = async () => {
-	try {
-	  setLoading(true);
-	  const response = await fetch(
-		`${process.env.NEXT_PUBLIC_BASE_URL}api/vehicle/${id}`,
-		{
-		  cache: "no-store",
-		  method: "GET",
-		  headers: {
-			"Content-Type": "application/json",
-		  },
+	const fetchVehicle = async () => {
+		try {
+			setLoading(true);
+			const response = await fetch(
+				`${process.env.NEXT_PUBLIC_BASE_URL}api/vehicle/${id}`,
+				{
+					cache: "no-store",
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+			const { data } = await response.json();
+			setVehicle(data);
+			setLoading(false);
+		} catch (error) {
+			console.log(error);
 		}
-	  );
-	  const { data } = await response.json();
-	  setVehicle(data);
-	  setLoading(false);
-	} catch (error) {
-	  console.log(error);
+	};
+	useEffect(() => {
+		if (selectedVehicle) {
+			fetchVehicle();
+		}
+	}, [selectedVehicle]);
+
+	if (loading) {
+		return <LoadingComponent />;
 	}
-  };
-  useEffect(() => {
-    if (selectedVehicle) {
-      fetchVehicle();
-    }
-  }, [selectedVehicle]);
+	const giftFood = async () => {
+		AddStatus(selectedVehicle);
 
-  if (loading) {
-    return <LoadingComponent />;
-  }
-  const giftFood = async () => {
-	AddStatus(selectedVehicle)
-	
-	fetchVehicle()
-  }
-  // Cek Status
-  let status = {};
-  if (vehicle) {
-    const { Status } = vehicle;
-    if (Status) {
-      status = Status;
-    }
-  }
+		fetchVehicle();
+	};
+	// Cek Status
+	let status = {};
+	if (vehicle) {
+		const { Status } = vehicle;
+		if (Status) {
+			status = Status;
+		}
+	}
 
-  let spareParts = [];
-  if (vehicle) {
-    const { Spareparts } = vehicle;
-    if (Spareparts) {
-      spareParts = Spareparts;
-    }
-  }
+	let spareParts = [];
+	if (vehicle) {
+		const { Spareparts } = vehicle;
+		if (Spareparts) {
+			spareParts = Spareparts;
+		}
+	}
 
-  return (
-    <>
-      <div className="flex justify-center">
-        <div className="flex w-full justify-center items-center">
-          <div className="flex-1 h-full flex justify-center items-end">
-            <div className="flex animate-bounce">
-              <img
-                src={vehicle?.image}
-                fillTransparencyColor={"transparent"}
-                pixelSize={12}
-                centered={true}
-              />
-            </div>
-          </div>
-          <div className="flex-1 h-full border">
-            <div className="">
-              <p className="text-white">Status :</p>
-              <p className="text-white">HP : {status[0]?.HP}</p>
-              <p className="text-white">Daily HP : {status[0]?.dailyHP}</p>
-              <p className="text-white">Coint : {status[0]?.cointReward}</p>
-              <br />
-              <p className="text-white">Spareparts :</p>
-              {spareParts &&
-                spareParts.map((el, i) => (
-                  <p className="text-white" key={i}>
-                    {el.name} : {vehicle.Books[0]?.serviceDate}
-                  </p>
-                ))}
-              <br />
-              <p className="text-white btn" onClick={giftFood}>Gift Food</p>
-              <p className="text-white btn" onClick={()=> changeGatcha(vehicle._id)}>HIT GATCHA</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="flex justify-center w-full">
+				<div className="flex w-full justify-center items-center">
+					<div className="w-1/2 h-full flex justify-center items-end ms-24">
+						<div className="flex animate-shake animate-infinite animate-duration-[1500ms]">
+							<img
+								src={vehicle?.image}
+								fillTransparencyColor={"transparent"}
+								pixelSize={12}
+								centered={true}
+							/>
+						</div>
+					</div>
+					<div className="w-1/2 h-full flex flex-col justify-center">
+						<div className="ps-24 pe-48 grid gap-7">
+							<div className="grid gap-1 text-white">
+								<p className="font-bold">Vehicle info :</p>
+								<p>Name : {vehicle.name}</p>
+								<p>Transmition : {vehicle.type}</p>
+							</div>
+							<div className="grid gap-1 text-white">
+								<p className="font-bold">Status :</p>
+								<p>HP : {status[0]?.HP}%</p>
+								<div
+									className={`w-[${Number(
+										status[0]?.HP
+									)}%] border h-2 rounded-full`}
+								/>
+								<p>Daily HP : {status[0]?.dailyHP}%</p>
+								<div
+									className={`w-[${Number(
+										status[0]?.dailyHP
+									)}%] border h-2 rounded-full`}
+								/>
+							</div>
+							<div className="grid gap-1">
+								<p className="text-white font-bold">Spareparts :</p>
+								{spareParts &&
+									spareParts.map((el, i) => (
+										<p className="text-white" key={i}>
+											{el.name} : {vehicle.Books[0]?.serviceDate}
+										</p>
+									))}
+								<br />
+								<div className="flex gap-5">
+									<button
+										className="text-white btn btn-outline w-24"
+										onClick={giftFood}
+									>
+										Gift Food
+									</button>
+									<button
+										className="text-white btn btn-outline w-24"
+										onClick={() => changeGatcha(vehicle._id)}
+									>
+										Hit Gatcha
+									</button>
+								</div>
+								<div className="text-white mt-5">
+									<p className="font-bold">Your Rewards</p>
+									<p>Coint : {status[0]?.cointReward}</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
